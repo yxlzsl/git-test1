4318ece4f96034cee61b390c3ae471469e5aa6ba
yxlzsl <90096100+yxlzsl@users.noreply.github.com>
2021-09-04 23:26:56 +0800

    pull commit

--- src/main/java/com/yxl/git/GitTest.java
+++ src/main/java/com/yxl/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix text");
         System.out.println("push text");
+        System.out.println("pull text");
+
     }
 }
